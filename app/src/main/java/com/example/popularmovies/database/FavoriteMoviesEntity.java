877d02b3c90411cd5d6a253b255bf104fc80219c
disha4mourya <disha4mourya@gmail.com>
2018-07-15 14:27:42 +0530

    room: entity for favorite movies

---
+++ app/src/main/java/com/example/popularmovies/database/FavoriteMoviesEntity.java
@@ -0,0 +1,157 @@
+package com.example.popularmovies.database;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+@Entity(tableName = "favorite")
+public class FavoriteMoviesEntity {
+
+    private String vote_count;
+    @PrimaryKey
+    private String id;
+    private String video;
+    private String vote_average;
+    private String title;
+    private String popularity;
+    private String poster_path;
+    private String original_language;
+    private String original_title;
+    private List<String> genre_ids;
+    private String backdrop_path;
+    private String adult;
+    private String overview;
+    private String release_date;
+
+    public FavoriteMoviesEntity(String vote_count, String id, String video, String vote_average, String title, String popularity, String poster_path, String original_language, String original_title, List<String> genre_ids, String backdrop_path, String adult, String overview, String release_date) {
+        this.vote_count = vote_count;
+        this.id = id;
+        this.video = video;
+        this.vote_average = vote_average;
+        this.title = title;
+        this.popularity = popularity;
+        this.poster_path = poster_path;
+        this.original_language = original_language;
+        this.original_title = original_title;
+        this.genre_ids = genre_ids;
+        this.backdrop_path = backdrop_path;
+        this.adult = adult;
+        this.overview = overview;
+        this.release_date = release_date;
+    }
+
+    public String getVote_count() {
+        return vote_count;
+    }
+
+    public void setVote_count(String vote_count) {
+        this.vote_count = vote_count;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+    public String getVote_average() {
+        return vote_average;
+    }
+
+    public void setVote_average(String vote_average) {
+        this.vote_average = vote_average;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(String popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+
+    public String getOriginal_language() {
+        return original_language;
+    }
+
+    public void setOriginal_language(String original_language) {
+        this.original_language = original_language;
+    }
+
+    public String getOriginal_title() {
+        return original_title;
+    }
+
+    public void setOriginal_title(String original_title) {
+        this.original_title = original_title;
+    }
+
+    public List<String> getGenre_ids() {
+        return genre_ids;
+    }
+
+    public void setGenre_ids(List<String> genre_ids) {
+        this.genre_ids = genre_ids;
+    }
+
+    public String getBackdrop_path() {
+        return backdrop_path;
+    }
+
+    public void setBackdrop_path(String backdrop_path) {
+        this.backdrop_path = backdrop_path;
+    }
+
+    public String getAdult() {
+        return adult;
+    }
+
+    public void setAdult(String adult) {
+        this.adult = adult;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+}
